3b483cbae2b7257c7f1daa35d905d8fc29d1764d
Juana Villalpando <juana.v14@gmail.com>
2020-08-13 11:10:07 -0700

    added user schema

--- models/User.js
+++ models/User.js
@@ -1,5 +1,27 @@
 const mongoose = require('mongoose');
 //will take in an object with all the fields that we want
 const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    //want to have an avatar option after they have logged in/signed up
+    avatar: {
+        type: String
+    },
+    date: {
+        type: Date,
+        dafault: Data.now
+    }
+});
 
-})
+module.exports = User = mongoose.model('user', UserSchema)
