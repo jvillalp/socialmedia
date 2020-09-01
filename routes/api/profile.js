086bef9dbec425944a294510a25c419525cb1ff2
Juana Villalpando <juana.v14@gmail.com>
2020-09-01 12:15:49 -0700

    created an individual profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+
+
 const auth = require('../../middleware/auth')
 const profile = require('../../models/Profile')
 const User = require('../../models/User');
@@ -22,5 +25,76 @@ router.get("/me", auth, async (req, res) => {
         res.status(500).send('Server Error')
     }
 });
+//@route          POST api/profile/
+//@desc           create or update a user profile
+//@acess value    Private (needs to send a toke, auth middleware required)
+router.post('/', [auth, [
+    check('status', 'Status is required').not().isEmpty(),
+    check('skills', 'Skills is required').not().isEmpty()
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+    const {
+        company,
+        location,
+        website,
+        bio,
+        skills,
+        status,
+        githubusername,
+        youtube,
+        twitter,
+        instagram,
+        linkedin,
+        facebook
+    } = req.body;
 
+    //build profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (location) profileFields.location = location;
+    if (status) profileFields.status = status;
+    if (website) profileFields.website = website;
+    if (bio) profileFields.bio = bio;
+
+
+
+    if (skills) {
+        profileFields.skills = skills.split(',').map(skill => skill.trim());
+    }
+    // console.log(profileFields.skills);
+
+    // res.send("Hello");
+    profileFields.social = {}
+    if (youtube) profileFields.social.youtube = youtube;
+    if (twitter) profileFields.social.twitter = twitter;
+
+    // console.log(profileFields.social.twitter)
+
+    try {
+        let profile = await Profile.findOne({ user: req.user.id });
+
+        if (profile) {
+            //update
+            profile = await Profile.findOneAndUpdate(
+                { user: req.user.id },
+                { $set: profileFields },
+                { new: true }
+            );
+            return res.json(profile)
+        }
+        //create 
+        profile = new Profile(profileFields);
+
+        await profile.save();
+        res.json(Profile);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error")
+    }
+
+})
 module.exports = router;
