044a74a8019b654c3836e0a6272b351fc5be893c
Juana Villalpando <juana.v14@gmail.com>
2020-09-01 11:19:02 -0700

    user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const router = express.Router();
-//@route          GET api/profile
-//@desc           Test Route
-//@acess value    Pubic (do not need a token will create auth middleware)
-router.get("/", (req, res) => res.send("Profile Route"));
+const auth = require('../../middleware/auth')
+const profile = require('../../models/Profile')
+const User = require('../../models/User');
+const Profile = require('../../models/Profile');
+//@route          GET api/profile/me
+//@desc           Get current users profile
+//@acess value    Private (needs to send a toke, auth middleware required)
+router.get("/me", auth, async (req, res) => {
+    try {
+        const progile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar']);
+
+        if (!profile) {
+            res.status(400).json({ msg: "There is no profile for this user" });
+        }
+
+        res.json(profile)
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error')
+    }
+});
 
 module.exports = router;
